7c64dccdb6c680506ae7a5facb790143e548d5b0
samKit88 <samuel.girma.cx9896@gmail.com>
2023-10-01 05:13:02 -0800

    0x00-pascal_triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    0-pascal_triangle.py: pascal_triangle()
+"""
+
+
+def pascal_triangle(n):
+    """
+        returns a lis of lists of integers
+        Args:
+            n (int): number of lists and digits
+        Returns: list of lists
+    """
+    final_triangle = [[1] * i for i in range(1, n + 1)]
+    i = 1
+    while i < n:
+        for j in range(1, len(final_triangle[i - 1])):
+            final_triangle[i][j] = \
+                final_triangle[i - 1][j] + \
+                final_triangle[i - 1][j - 1]
+        i += 1
+    return final_triangle
