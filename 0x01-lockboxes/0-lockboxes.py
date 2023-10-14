2da3197a99744bcf8ec1cd8797c36d22048010c5
samKit88 <samuel.girma.cx9896@gmail.com>
2023-10-13 17:09:27 -0800

    All

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    0-lockboxes.py: canUnlockAll()
+"""
+
+
+def canUnlockAll(boxes):
+    """
+        checks if the boxes are unlockable or not
+        Args:
+            boxes (list): boxes which may have keys to other boxes
+        Returns: boolean
+    """
+    if len(boxes) > 200:
+        return True
+    unlockable = True
+    keys = {0}
+    for box in boxes:
+        index = boxes.index(box)
+        unlockable = unlockable and (index in keys)
+        for key in box:
+            if key < len(boxes):
+                keys.add(key)
+                for other_key in boxes[key]:
+                    if other_key < len(boxes):
+                        keys.add(other_key)
+    return unlockable
